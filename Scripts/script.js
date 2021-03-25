5db393baeb070357a216331bf05bedd806d1b624
Bleyk <50923316+pedro-ca@users.noreply.github.com>
2021-03-25 16:47:31 -0300

    Sistema de colisão WIP (ainda com erro)

--- Scripts/script.js
+++ Scripts/script.js
@@ -15,9 +15,46 @@ camera.position.z = 60;
 camera.position.y = 2;
 camera.position.x = -55;
 
+//Debug Collision
+var cubeGeometry = new THREE.BoxGeometry(2,2,2,1,1,1);
+var wireMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe:true } )
+var PlayerHitbox = new THREE.Mesh( cubeGeometry, wireMaterial );
+PlayerHitbox.position.set(-55,2,60)
+cena.add(PlayerHitbox)
+
+var hitbox2 = new THREE.Mesh(new THREE.BoxGeometry(1.2,2,1), new THREE.MeshLambertMaterial({color: 0xff4000}));
+hitbox2.position.y = 2;
+hitbox2.position.x = -55;
+hitbox2.position.z = 50;
+cena.add(hitbox2);
 
 function desenhar(){
     render.render(cena, camera);
     requestAnimationFrame(desenhar);
+
+    PlayerHitbox.position.set(camera.position.x, camera.position.y, camera.position.z);
+    if(checkCollision()){
+        console.log("Colisão");
+    }
+    //console.log(Player.position.x +","+Player.position.y+","+Player.position.z);
 }
 requestAnimationFrame(desenhar);
+
+function checkCollision(){
+    var originPoint = PlayerHitbox.position.clone();
+
+    for (var vertexIndex = 0; vertexIndex < PlayerHitbox.geometry.vertices.length; vertexIndex++)
+    {       
+        var localVertex = PlayerHitbox.geometry.vertices[vertexIndex].clone();
+        var globalVertex = PlayerHitbox.matrix.multiplyVector3(localVertex);
+        var directionVector = globalVertex.subSelf( PlayerHitbox.position );
+
+        var ray = new THREE.Ray( PlayerHitbox.position, directionVector.clone().normalize() );
+        var collisionResults = ray.intersectObjects( collidableMeshList );
+        if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() ) 
+        {
+            return true;
+        }
+    }
+    return false;
+}
