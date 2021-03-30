f367d3871e0160c00b6ee0b9e34dbb03e7ef61c1
Bleyk <50923316+pedro-ca@users.noreply.github.com>
2021-03-30 16:53:25 -0300

    Corrigindo colisão para aceitar qualquer formato de retangulo
    Lógica de colisão= RectA.Left < RectB.Right && RectA.Right > RectB.Left &&RectA.Top > RectB.Bottom && RectA.Bottom < RectB.Top )

--- Scripts/PlayerCollision.js
+++ Scripts/PlayerCollision.js
@@ -9,7 +9,7 @@ if(debugMode == true){
 }
 
 //Hitbox de Debug (p/ testar colisão. Deve ser descartado depois)
-var MonsterHitbox = new THREE.Mesh(new THREE.BoxGeometry(1.2,2,2), new THREE.MeshLambertMaterial({color: 0xff4000}));
+var MonsterHitbox = new THREE.Mesh(new THREE.BoxGeometry(2,2,2), new THREE.MeshLambertMaterial({color: 0xff4000}));
 MonsterHitbox.position.y = 2;
 MonsterHitbox.position.x = -55;
 MonsterHitbox.position.z = 50;
@@ -17,21 +17,19 @@ if(debugMode == true){
     cena.add(MonsterHitbox);
 }
 
-
 //Método de colisão entre duas "THREE.Mesh". NOTA: essa colisão NÃO leva em consideração o eixo Y.
 function checkCollision(mesh1, mesh2){
     var m1Xposition = mesh1.position.x;
     var m1ZPosition = mesh1.position.z;
-    var m1Width = mesh1.geometry.parameters.width;
-    var m1Depth = mesh1.geometry.parameters.depth;
+    var m1Width = mesh1.geometry.parameters.width;  //width — Width; the length of the edges parallel to the X axis.
+    var m1Depth = mesh1.geometry.parameters.depth;  //depth — Depth; the length of the edges parallel to the Z axis.
 
     var m2Xposition = mesh2.position.x;
     var m2ZPosition = mesh2.position.z;
     var m2Width = mesh2.geometry.parameters.width;
     var m2Depth = mesh2.geometry.parameters.depth;
     
-    return !(m1Xposition > m2Xposition + m2Width 
-        || m1Xposition + m1Width < m2Xposition 
-        || m1ZPosition > m2ZPosition + m2Depth 
-        || m1ZPosition + m1Depth < m2ZPosition);
+    return (m1Xposition-m1Width/2 < m2Xposition+m2Width/2 && m1Xposition+m1Width/2 >  m2Xposition-m2Width/2
+        && m1ZPosition+m1Depth/2 > m2ZPosition-m2Depth/2 && m1ZPosition-m1Depth/2 < m2ZPosition+m2Depth/2 )
+    
 }
